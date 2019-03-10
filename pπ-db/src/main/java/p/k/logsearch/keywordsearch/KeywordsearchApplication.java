63aacb909b662bcb55bc7a2c8a337fea4b6a3270
kenail2002 <kenail2002@gmail.com>
2019-03-10 21:42:46 +0800

    spring webflow

---
+++ pπ-db/src/main/java/p/k/logsearch/keywordsearch/KeywordsearchApplication.java
@@ -0,0 +1,13 @@
+package p.k.logsearch.keywordsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordsearchApplication.class, args);
+	}
+
+}
