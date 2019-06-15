5d7d91e434375ad7717ffc2f6bcee8929049c639
kenail2002 <kenail2002@gmail.com>
2019-06-15 12:12:30 +0800

    db query page

--- tasklist/src/main/kotlin/com/ppai/timemgr/tasklist/main/TaskController.kt
+++ tasklist/src/main/kotlin/com/ppai/timemgr/tasklist/main/TaskController.kt
@@ -1,6 +1,8 @@
 package com.ppai.timemgr.tasklist.main
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.*
 import org.springframework.web.bind.annotation.*
@@ -8,18 +10,30 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.util.*
 
+
 @RestController
 class TaskController(@Autowired private val repository: TaskRepository) {
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/task", consumes = arrayOf(APPLICATION_STREAM_JSON_VALUE, APPLICATION_JSON_VALUE,APPLICATION_FORM_URLENCODED_VALUE+";charset=UTF-8"))
-    internal fun create(@RequestBody personStream: Task ): Any {
+    @PostMapping("/task", consumes = arrayOf(APPLICATION_STREAM_JSON_VALUE, APPLICATION_JSON_VALUE, APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8"))
+    internal fun create(@RequestBody personStream: Task): Any {
         return this.repository.save(personStream)
     }
 
+    /*
+    page: 1
+    rows: 10
+    sort: id
+    order: desc
+     */
     @PostMapping("/tasklist")
-    internal fun list(): Flux<Task> {
-        return this.repository.findAll()
+    internal fun list(@RequestParam("page") currentPage: Int,
+                      @RequestParam("rows") pageSize: Int,
+                      @RequestParam("sort") sort: String): Flux<Task> {
+
+        val pageable = PageRequest(currentPage-1, pageSize, Sort.Direction.DESC, sort)
+        println("pageData:$pageable")
+        return this.repository.pageQuery(pageable)
     }
 
     @GetMapping("/task/{id}")
--- tasklist/src/main/kotlin/com/ppai/timemgr/tasklist/main/TaskRepository.kt
+++ tasklist/src/main/kotlin/com/ppai/timemgr/tasklist/main/TaskRepository.kt
@@ -1,6 +1,7 @@
 package com.ppai.timemgr.tasklist.main
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -28,5 +29,9 @@ class TaskRepository(@Autowired private val taskRepo: TaskRepo) {
     fun findOne(id: Long): Mono<Optional<Task>> {
         System.out.println("findOne")
         return (taskRepo.findById(id)).toMono()
+    }
+
+    fun pageQuery(pageData: Pageable): Flux<Task> {
+        return (taskRepo.findAll(pageData)).toFlux()
     }
 }
