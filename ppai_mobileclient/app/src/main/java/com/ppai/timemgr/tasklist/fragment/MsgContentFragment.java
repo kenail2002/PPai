6f1ee085539d07e4946d5550d1535bddaef305e0
kenail2002 <kenail2002@gmail.com>
2019-11-10 20:59:06 +0800

    调整弹出菜单位置

--- ppai_mobileclient/app/src/main/java/com/ppai/timemgr/tasklist/fragment/MsgContentFragment.java
+++ ppai_mobileclient/app/src/main/java/com/ppai/timemgr/tasklist/fragment/MsgContentFragment.java
@@ -1,14 +1,17 @@
 package com.ppai.timemgr.tasklist.fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.view.menu.MenuPopupHelper;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +21,7 @@ import com.ppai.timemgr.tasklist.TaskDetailFragment;
 import com.ppai.timemgr.tasklist.bean.TaskEntity;
 import com.ppai.timemgr.tasklist.bean.TasksContainer;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 /**
@@ -46,13 +50,51 @@ public class MsgContentFragment extends Fragment {
         ButterKnife.bind(this, view);
         tvContent.setText(name);
         View inflate = inflater.inflate(R.layout.item_list, null);
-        ((RecyclerView) inflate).setAdapter(new TasksRecylerViewAdapter(this, TasksContainer.ITEMS, false));
+        final RecyclerView cv1 = (RecyclerView) inflate;
+        cv1.setAdapter(new TasksRecylerViewAdapter(this, TasksContainer.ITEMS, false));
+        cv1.addOnItemTouchListener(new OnRecyclerItemClickListener(cv1) {
+
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onLongClick(RecyclerView.ViewHolder vh) {
+//                Snackbar.make(cv1, "你在长按着任务呢", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                //创建弹出式菜单对象（最低版本11）
+                PopupMenu popup = new PopupMenu(cv1.getContext(), cv1);//第二个参数是绑定的那个view
+                //获取菜单填充器
+                MenuInflater inflater = popup.getMenuInflater();
+                //填充菜单
+                inflater.inflate(R.menu.menu_edit_task, popup.getMenu());
+                //绑定菜单项的点击事件
+                popup.setOnMenuItemClickListener(this);
+                //显示(这一行代码不要忘记了)
+                Field field = null;
+                try {
+                    field = popup.getClass().getDeclaredField("mPopup");
+                    field.setAccessible(true);
+
+                    MenuPopupHelper helper = (MenuPopupHelper) field.get(popup);
+                    y = y - cv1.getHeight();//如果y取的是触摸点的位置，可能需要作此处理，经测试android5.1的设备会弹窗在屏幕之外
+                    helper.show((int) x, (int) y);
+                } catch (NoSuchFieldException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+
+            public void onItemClick(RecyclerView.ViewHolder vh) {
+
+                //单击事件
+
+            }
+
+        });
 
         return inflate;
     }
 
-
-    public static class TasksRecylerViewAdapter
+    private static class TasksRecylerViewAdapter
             extends RecyclerView.Adapter<TasksRecylerViewAdapter.ViewHolder> {
 
         private final Fragment mParentActivity;
@@ -119,6 +161,121 @@ public class MsgContentFragment extends Fragment {
                 mContentView = (TextView) view.findViewById(R.id.content);
             }
         }
+    }
+
+    private class OnRecyclerItemClickListener implements RecyclerView.OnItemTouchListener, PopupMenu.OnMenuItemClickListener {
+
+        float x = 0;
+        float y = 0;
+        private GestureDetectorCompat mGestureDetector;
+        private RecyclerView recyclerView;
+
+        public OnRecyclerItemClickListener(RecyclerView recyclerView) {
+
+            this.recyclerView = recyclerView;
+
+            mGestureDetector = new GestureDetectorCompat(recyclerView.getContext(), new ItemTouchHelperGestureListener());
+
+        }
+
+
+        @Override
+
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            mGestureDetector.onTouchEvent(e);
+            x = e.getX();
+            y = e.getY();
+
+            return false;
+
+        }
+
+
+        @Override
+
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            mGestureDetector.onTouchEvent(e);
+
+        }
+
+
+        @Override
+
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            // TODO Auto-generated method stub
+            switch (item.getItemId()) {
+                case R.id.newTaks:
+                    Snackbar.make(recyclerView, "新建任务", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    break;
+                case R.id.editTask:
+                    Snackbar.make(recyclerView, "修改任务", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    break;
+                case R.id.deleteTask:
+                    Snackbar.make(recyclerView, "删除任务", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+
+        public void onLongClick(RecyclerView.ViewHolder vh) {
+        }
+
+        public void onItemClick(RecyclerView.ViewHolder vh) {
+        }
+
+        private class ItemTouchHelperGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+
+            @Override
+
+            public boolean onSingleTapUp(MotionEvent e) {
+
+                View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+
+                if (child != null) {
+
+                    RecyclerView.ViewHolder vh = recyclerView.getChildViewHolder(child);
+
+                    onItemClick(vh);
+
+                }
+
+                return true;
+
+            }
+
+
+            @Override
+
+            public void onLongPress(MotionEvent e) {
+
+                View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+
+                if (child != null) {
+
+                    RecyclerView.ViewHolder vh = recyclerView.getChildViewHolder(child);
+
+                    onLongClick(vh);
+
+                }
+
+            }
+
+        }
+
     }
 
 }
